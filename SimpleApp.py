1e3db12d603f1efab298ab589e099981e6ee296a
haualan <alan.wc.hau@gmail.com>
2015-03-11 01:40:02 -0400

    ...

--- SimpleApp.py
+++ SimpleApp.py
@@ -11,27 +11,27 @@ conf = SparkConf().setAppName(appName).setMaster(master)
 sc = SparkContext(conf=conf)
 
 datafiles = [
-'googlebooks-eng-all-1gram-20120701-a',
-'googlebooks-eng-all-1gram-20120701-b',
-'googlebooks-eng-all-1gram-20120701-c',
-'googlebooks-eng-all-1gram-20120701-d',
-'googlebooks-eng-all-1gram-20120701-e', 
-'googlebooks-eng-all-1gram-20120701-f',
-'googlebooks-eng-all-1gram-20120701-g',
-'googlebooks-eng-all-1gram-20120701-h',
-'googlebooks-eng-all-1gram-20120701-i',
-'googlebooks-eng-all-1gram-20120701-j',
-'googlebooks-eng-all-1gram-20120701-k',
-'googlebooks-eng-all-1gram-20120701-l',
-'googlebooks-eng-all-1gram-20120701-m',
-'googlebooks-eng-all-1gram-20120701-n',
-'googlebooks-eng-all-1gram-20120701-o',
-'googlebooks-eng-all-1gram-20120701-other',
-'googlebooks-eng-all-1gram-20120701-p',
-'googlebooks-eng-all-1gram-20120701-q',
-'googlebooks-eng-all-1gram-20120701-r',
-'googlebooks-eng-all-1gram-20120701-s',
-'googlebooks-eng-all-1gram-20120701-t',
+# 'googlebooks-eng-all-1gram-20120701-a',
+# 'googlebooks-eng-all-1gram-20120701-b',
+# 'googlebooks-eng-all-1gram-20120701-c',
+# 'googlebooks-eng-all-1gram-20120701-d',
+# 'googlebooks-eng-all-1gram-20120701-e', 
+# 'googlebooks-eng-all-1gram-20120701-f',
+# 'googlebooks-eng-all-1gram-20120701-g',
+# 'googlebooks-eng-all-1gram-20120701-h',
+# 'googlebooks-eng-all-1gram-20120701-i',
+# 'googlebooks-eng-all-1gram-20120701-j',
+# 'googlebooks-eng-all-1gram-20120701-k',
+# 'googlebooks-eng-all-1gram-20120701-l',
+# 'googlebooks-eng-all-1gram-20120701-m',
+# 'googlebooks-eng-all-1gram-20120701-n',
+# 'googlebooks-eng-all-1gram-20120701-o',
+# 'googlebooks-eng-all-1gram-20120701-other',
+# 'googlebooks-eng-all-1gram-20120701-p',
+# 'googlebooks-eng-all-1gram-20120701-q',
+# 'googlebooks-eng-all-1gram-20120701-r',
+# 'googlebooks-eng-all-1gram-20120701-s',
+# 'googlebooks-eng-all-1gram-20120701-t',
 'googlebooks-eng-all-1gram-20120701-u',
 'googlebooks-eng-all-1gram-20120701-v',
 'googlebooks-eng-all-1gram-20120701-w',
@@ -200,7 +200,7 @@ if __name__ == "__main__":
 
   exportToFile(results,'all_correl.txt')
 
-  # print "map_correlation", task2.map_correlation()
+  print "map_correlation", task2.map_correlation()
 
 
 
